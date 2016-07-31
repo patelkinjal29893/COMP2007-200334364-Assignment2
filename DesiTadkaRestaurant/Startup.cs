a0a4e17264cf4fd20596bc33545b89235b3864bf
Kinjal Patel <patelkinjal298@hotmail.com>
2016-07-31 19:57:04 -0400

    Add project files.

---
+++ DesiTadkaRestaurant/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DesiTadkaRestaurant.Startup))]
+namespace DesiTadkaRestaurant
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
